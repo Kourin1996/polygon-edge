c9102470b253d6a28b1d85d26537d268e77b1416
kourin <kourin.code@gmail.com>
2022-07-03 10:16:11 +0900

    Remove sync.Mutex

--- e2e/framework/testserver.go
+++ e2e/framework/testserver.go
@@ -64,10 +64,6 @@ type TestServer struct {
 }
 
 func NewTestServer(t *testing.T, rootDir string, callback TestServerConfigCallback) *TestServer {
-	// To lock for the ports
-	hoge.Lock()
-	defer hoge.Unlock()
-
 	t.Helper()
 
 	// Reserve ports
